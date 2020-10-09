ed98ff01b30a71a55eae0f17f8a9c25adb565417
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-10-09 15:33:45 -0500

    created file directories for components so everyone has same file system

---
+++ src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => (
+    <>
+        <h2>Bad Reactors</h2>
+        <small>Where we don't give a 💩</small>
+
+        <address>
+            <div>Visit Us at the Nunya Biznazzzzz</div>
+            <div>500 Puppy Way</div>
+        </address>
+    </>
+)
