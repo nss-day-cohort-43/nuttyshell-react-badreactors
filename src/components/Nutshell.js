53b7f4a3454332c7fbf95e5cb83ce73154ecceb3
Devco2011 <devco2011@gmail.com>
2020-10-11 12:35:52 -0500

    beginnings of auth

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import "./Nutshell.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const Nutshell = () => (
+  <>
+    <Route render={() => {
+      if (localStorage.getItem("nutshell_user")) {
+        return (
+          <>
 
-export default App;
+            <ApplicationViews />
+          </>
+        )
+      } else {
+        return <Redirect to="/login" />
+      }
+    }} />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+)
