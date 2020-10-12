2368a95e6a1620ea72ba6c7cc0a618b08d53fe5a
Devco2011 <devco2011@gmail.com>
2020-10-12 16:26:47 -0500

    a little styling on articles list and add article form.

--- src/components/article/ArticleCard.js
+++ src/components/article/ArticleCard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Article.css"
+import { ListGroup, ListGroupItem } from 'reactstrap'
 import { Link } from "react-router-dom"
 
 
@@ -7,10 +8,17 @@ import { Link } from "react-router-dom"
 
 
 export const ArticleCard = ({ article }) => (
-    <section className="article">
-        <Link to={`/articles/detail/${article.id}`}>
+    <ListGroup>
+        <ListGroupItem tag="button" action><Link to={`/articles/detail/${article.id}`}>
             <h3 className="article__title">{article.title}</h3>
-        </Link>
-        <div className="article__userId">Posted by: {article.user?.username} on {article.date}</div>
-    </section>
+            <div className="article__userId">Posted by: {article.user?.username} on {article.date}</div>
+        </Link></ListGroupItem>
+    </ListGroup>
 )
+    // <section className="article">
+    //< Link to = {`/articles/detail/${article.id}`}>
+       // <h3 className="article__title">{article.title}</h3>
+       // </Link >
+      //  <div className="article__userId">Posted by: {article.user?.username} on {article.date}</div>
+   // </section>
+//)
