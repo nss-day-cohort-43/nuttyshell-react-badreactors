0de663b429feea289fc7d45d19072f59ad9202b1
Tessa Steffen <tessasofteng@gmail.com>
2020-10-12 09:20:03 -0500

    Application views added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,13 +2,28 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 
+//Tasks//
+import { TaskProvider } from "./task/TaskProvider";
+import { TaskList } from "./task/TaskList";
+import { TaskForm } from "./task/TaskForm";
+import { TaskDetail} from "./task/TaskDetail";
+
 export const ApplicationViews = () => {
     return (
         <>
-            {/* Render the location list when http://localhost:3000/ */}
+     {/* Render the location list when http://localhost:3000/ */}
             <Route exact path="/">
                 <Home />
             </Route>
+
+
+{/*Tasks*/}
+     {/*Render Task Form when http://localhost:3000/tasks*/}  
+            <TaskProvider>
+                <Route exact path="/tasks"
+                    <TaskList />
+                </Route>
+            </TaskProvider>
         </>
     )
 }
